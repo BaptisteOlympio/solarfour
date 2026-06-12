96443aeac9eed3d2a41f527317cd0cbd49adf3a6
BaptisteOlympio <baptiste.olympio@gmail.com>
2026-06-12 23:07:10 +0200

    move backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,17 +2,26 @@ import asyncio
 import struct
 from asyncio import Queue
 from contextlib import asynccontextmanager
+from dataclasses import dataclass
 from typing import Dict
 
 import serial
 import uvicorn
 from fastapi import FastAPI
 
+
+@dataclass
+class Angles:
+    theta: float
+    phi: float
+
+
 command_queue: Queue = Queue()
-ARDUINO_UNO = serial.Serial(port="COM7", baudrate=9600, timeout=0.1)
+ARDUINO_UNO = serial.Serial(port="COM7", baudrate=9600, timeout=5)
 
 
 async def send_angle(theta: float, phi: float) -> bytes:
+    print("Send data")
     binary_data = struct.pack("<ff", theta, phi)
     await asyncio.to_thread(ARDUINO_UNO.write, binary_data)
     data = await asyncio.to_thread(ARDUINO_UNO.readline)
@@ -22,9 +31,9 @@ async def send_angle(theta: float, phi: float) -> bytes:
 async def queue_worker():
     print("Worker started, serial port open:", ARDUINO_UNO.is_open)
     while True:
-        command: Dict[str, float] = await command_queue.get()
-        theta = float(command["theta"])
-        phi = float(command["phi"])
+        command: Angles = await command_queue.get()
+        theta = command.theta
+        phi = command.phi
         print(f"Sending theta={theta} phi={phi}")
         response = await send_angle(theta=theta, phi=phi)
         print(f"Arduino response: {response}")
@@ -34,7 +43,9 @@ async def queue_worker():
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     task = asyncio.create_task(queue_worker())
+    print("Done")
     yield
+    print("Task cancel")
     task.cancel()
     ARDUINO_UNO.close()
 
@@ -42,10 +53,11 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/")
-async def test():
-    await command_queue.put({"theta": 5.0, "phi": 5.0})
-    return {"theta": 5, "phi": 5}
+@app.get("/moveby")
+async def test(theta: float, phi: float):
+    command = Angles(theta=theta, phi=phi)
+    await command_queue.put(command)
+    return {"theta": theta, "phi": phi}
 
 
 if __name__ == "__main__":
